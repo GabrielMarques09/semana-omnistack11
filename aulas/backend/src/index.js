c61d779dbe0c9d18088378b4594c1be6ed04b936
Gabriel <gabrielmarrques@hotmail.com>
2020-03-26 23:12:47 -0300

    first commit

---
+++ aulas/backend/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express()
+
+app.use(cors())
+app.use(express.json());
+app.use(routes)
+
+/**
+ * Query Params: Parâmetros nomeados, enviados na rota após o "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+app.listen(3333);
